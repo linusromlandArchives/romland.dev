6ca75a4fb1936a0497e9704a0957211e933b7d78
Linus Romland <romlandlinus@gmail.com>
2022-05-17 17:11:20 +0200

    Small fix

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,6 +1,5 @@
 //External dependencies import
-import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 //Views import
 import Home from './views/Home';
