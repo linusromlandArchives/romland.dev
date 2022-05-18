19a3ab5e812a6f4a1c9c418ab9db389889f45fc0
Linus Romland <romlandlinus@gmail.com>
2022-05-18 13:44:43 +0200

    Changed auth check to use passport

--- backend/src/auth/index.ts
+++ backend/src/auth/index.ts
@@ -2,13 +2,11 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function checkAdmin(req: Request, res: Response, next: NextFunction) {
-    const password = req.body.password ? req.body.password : req.query.password;
-    if (password == 'password') {
-        next();
-    } else {
-        res.status(403).json({
-            success: false,
-            error: 'Access denied',
-        });
+    if (req.isAuthenticated()) {
+        return next();
     }
+    res.status(401).json({
+        success: false,
+        error: 'Access denied',
+    });
 }
