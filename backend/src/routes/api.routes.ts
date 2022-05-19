0c28c2d0ad78c6fdc1c7a1549a94d5cfb48c7fc1
Linus Romland <romlandlinus@gmail.com>
2022-05-19 14:52:48 +0200

    Added a wildcard catch for all routes to /api

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -13,4 +13,19 @@ router.use('/project', projectRoutes);
 router.use('/projectImage', projectImage);
 router.use('/auth', authRoutes);
 
+router.get('/', (req, res) => {
+    res.status(200).json({
+        success: true,
+        error: '',
+        message: 'Welcome to the Romland.dev API!',
+    });
+});
+
+router.all('*', async (_req, res) => {
+    res.status(404).json({
+        success: false,
+        error: 'Invalid endpoint',
+    });
+});
+
 export default router;
