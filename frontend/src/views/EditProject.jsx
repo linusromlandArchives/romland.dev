c9574b765df874dd511094f9598fc9b6748a7527
Linus Romland <romlandlinus@gmail.com>
2022-05-24 11:11:32 +0200

    Started on image showing and deletion

--- frontend/src/views/EditProject.jsx
+++ frontend/src/views/EditProject.jsx
@@ -1,12 +1,13 @@
 //External dependencies import
 import Select from 'react-select';
+import { toast } from 'react-toastify';
 import { Field, ErrorMessage, Form, Formik } from 'formik';
 import { Link, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 //Local dependencies import
 import axios from '../axios';
-import { successNotify, errorNotify } from '../components/Toast';
+import { successNotify, infoNotify, errorNotify } from '../components/Toast';
 
 export default () => {
     const { projectID } = useParams();
@@ -14,11 +15,44 @@ export default () => {
     const [languages, setLanguages] = useState([]);
     const [activeLanguages, setActiveLanguages] = useState([]);
     const [project, setProject] = useState({});
+    const [deleteImageID, setDeleteImageID] = useState('');
+    const [deleteToastID, setDeleteToastID] = useState('');
 
     useEffect(() => {
         getLanguages();
         getProject();
     }, []);
+
+    async function deleteProjectImage(projectImagesID) {
+        if (deleteImageID == projectImagesID) {
+            setDeleteImageID('');
+            toast.dismiss(deleteToastID);
+            const request = await axios.delete('/api/projectImage', { data: { projectImagesID } });
+            const response = await request.data;
+            if (response.success) {
+                successNotify('Image deleted successfully', {
+                    theme: 'colored',
+                });
+                getProject();
+            } else {
+                errorNotify('Image deletion failed', {
+                    theme: 'colored',
+                });
+            }
+        } else {
+            toast.dismiss(deleteToastID);
+            setDeleteToastID(
+                infoNotify('Click again to delete image', {
+                    duration: 3000,
+                    theme: 'colored',
+                }),
+            );
+            setDeleteImageID(projectImagesID);
+            setTimeout(() => {
+                setDeleteImageID('');
+            }, 3000);
+        }
+    }
 
     async function getProject() {
         const request = await axios.get(`/api/project/`, { body: { ids: projectID } });
@@ -54,7 +88,7 @@ export default () => {
 
     return (
         <div className="bg-teal-700 min-h-screen w-screen flex items-center justify-center">
-            <div className="w-full md:w-1/2 rounded-md bg-slate-200 p-4">
+            <div className="w-full lg:w-1/2 rounded-md bg-slate-200 p-4">
                 <div className="flex justify-between items-center">
                     <h1 className="text-4xl font-semibold">Edit a project</h1>
                     <Link
@@ -64,7 +98,7 @@ export default () => {
                         Back to admin
                     </Link>
                 </div>
-                <div className="flex justify-between items-center">
+                <div className="flex justify-center flex-col md:flex-row">
                     <Formik
                         enableReinitialize={true}
                         initialValues={{
@@ -142,7 +176,7 @@ export default () => {
                         }}
                     >
                         {({ values, setFieldValue, isValid }) => (
-                            <Form className="flex flex-col">
+                            <Form className="flex flex-col w-full md:w-1/2 mr-0 md:mr-6 mt-4 md:mt-0">
                                 <label className="flex flex-col">
                                     Project name
                                     <Field
@@ -214,8 +248,33 @@ export default () => {
                             </Form>
                         )}
                     </Formik>
-                    <div>
-                        <h1>Project images here</h1>
+                    <div className="w-full md:w-1/2 ml-0 md:ml-6">
+                        <button className="bg-blue-500 hover:bg-blue-400 disabled:bg-blue-300 m-2 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition ease duration-150 cursor-pointer disabled:cursor-not-allowed">
+                            Upload Image
+                        </button>
+
+                        <div className="flex flex-wrap">
+                            {project &&
+                                project.projectImages &&
+                                project.projectImages.length > 0 &&
+                                project.projectImages.map((image, index) => (
+                                    <div key={index} className="w-5/12 m-2 flex flex-col items-center bg-slate-300 rounded-md p-2">
+                                        <img
+                                            src={'/api/projectImage/' + image.projectImagesID}
+                                            alt={image.projectImagesFileName}
+                                            className="h-28 rounded-md"
+                                        />
+                                        <button
+                                            className="bg-red-500 hover:bg-red-400 disabled:bg-red-300 m-2 text-white font-semibold p-1 rounded focus:outline-none focus:shadow-outline transition ease duration-150 cursor-pointer disabled:cursor-not-allowed flex items-center w-full justify-center"
+                                            onClick={() => {
+                                                deleteProjectImage(image.projectImagesID);
+                                            }}
+                                        >
+                                            <p>Delete</p>
+                                        </button>
+                                    </div>
+                                ))}
+                        </div>
                     </div>
                 </div>
             </div>
