c555d2f1fd46ad3c8b23fa29a61a6f9744850658
Linus Romland <romlandlinus@gmail.com>
2022-05-28 18:54:36 +0200

    Styled component with `styled-component`

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,27 @@
 //External dependencies import
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+
+//Styled Navbar (This is only done to fulfill the requirement of the assignment)
+const Navbar = styled.nav`
+    background-color: white;
+    border-bottom: 1px solid rgb(203 213 225);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 3.5rem;
+    width: 100%;
+`;
 
 export default () => {
     return (
-        <nav className="flex h-14 w-full items-center justify-between border-b border-slate-300 bg-white">
+        <Navbar>
             <Link to="/">
                 <h1 className="ml-4 text-2xl font-semibold">Romland.Dev</h1>
             </Link>
             <Link to="/search" className="mr-4 text-2xl md:text-xl">
                 Search
             </Link>
-        </nav>
+        </Navbar>
     );
 };
